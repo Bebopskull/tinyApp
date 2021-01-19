8e8d78f5f61ca8182ae78a871cce6869b229b350
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-01-18 21:58:56 -0500

    added the post router for adding new URLs and implemented a function to randomly generate strings to use as shortened verions of the urls

--- express_server.js
+++ express_server.js
@@ -2,17 +2,31 @@ const { response } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+///generate random string
+let generateRandomString = function makeid(length = 6) {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+// console.log(makeid(5));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-///route handlers
 
-
+///ROUTE HANDLERS///
 
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
@@ -30,6 +44,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+/////route to the 'input new url page'///
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 ///shows a single shorthened_url and its url equivalent
 app.get("/urls/:shortURL", (req, res) => {
   
@@ -38,18 +57,30 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
+});
  
- app.get("/fetch", (req, res) => {
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+});
+
+
+/////posts///// 
+
+/////submit new url
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
---
+++ randomString.js
@@ -0,0 +1,11 @@
+let generateRandomString = function makeid(length = 6) {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+console.log(generateRandomString(6));
